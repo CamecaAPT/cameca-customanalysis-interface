09ecf3970ffe1c52ccaf6580fd59e14dd9305614
Jim Payne <jim.payne@ametek.com>
2022-04-11 15:00:43 -0500

    Add IonData facade
    IIonDataFacade will eventually replace IIonData

---
+++ Cameca.CustomAnalysis.Interface/IonData/IIonDataFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cameca.CustomAnalysis.Interface.IonData;
+
+public interface IIonDataFacade
+{
+    void AddVirtualSection(string section, Type type, string unit = "", uint valuesPerRecord = 1);
+    public void IterateSectionData(IonDataChunkDelegate chunkDelegate);
+    public void IterateSectionData(IonDataChunkDelegate chunkDelegate, IEnumerable<string> sections);
+}
