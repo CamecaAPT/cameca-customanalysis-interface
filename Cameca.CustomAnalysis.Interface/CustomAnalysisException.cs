ec709c7ee816f9c075149f60b1f2047c003a1d34
Jim Payne <jim.payne@ametek.com>
2022-05-23 13:59:39 -0500

    Add public exceptions
    * Add base exception that custom extension should derive from
    * Add exception for when requested ion data section is not found

---
+++ Cameca.CustomAnalysis.Interface/CustomAnalysisException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public class CustomAnalysisException : Exception
+{
+    public CustomAnalysisException()
+        : base()
+    {
+    }
+
+    public CustomAnalysisException(string message)
+        : base(message)
+    {
+    }
+
+
+    public CustomAnalysisException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
