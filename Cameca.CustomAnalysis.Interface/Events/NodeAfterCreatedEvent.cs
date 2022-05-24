a89b6407357a1d9bc4fe682ff4421f0c765a4d71
Jim Payne <jim.payne@ametek.com>
2022-05-24 11:26:51 -0500

    Add node after creation event

---
+++ Cameca.CustomAnalysis.Interface/Events/NodeAfterCreatedEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Prism.Events;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public class NodeAfterCreatedEvent : PubSubEvent<NodeAfterCreatedEventArgs> { }
+
+public class NodeAfterCreatedEventArgs : INodeTargetEvent
+{
+	public Guid NodeId { get; }
+
+	public NodeAfterCreatedEventArgs(Guid analysisNodeId)
+	{
+		NodeId = analysisNodeId;
+	}
+}
+
+public static class NodeAfterCreatedEventExtensions
+{
+	public static void PublishNodeCreated(
+		this IEventAggregator eventAggregator,
+		Guid nodeId)
+	{
+		eventAggregator
+			.GetEvent<NodeAfterCreatedEvent>()
+			.Publish(new NodeAfterCreatedEventArgs(nodeId));
+	}
+
+	public static SubscriptionToken SubscribeNodeCreated(
+		this IEventAggregator eventAggregator,
+		Action<NodeAfterCreatedEventArgs> action,
+		Predicate<NodeAfterCreatedEventArgs>? filter = null)
+	{
+		return eventAggregator
+			.GetEvent<NodeAfterCreatedEvent>()
+			.Subscribe(action, filter: filter);
+	}
+}
+
