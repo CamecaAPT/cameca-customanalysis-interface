061914febce52972abfc81637253e1a128d870c3
Jim Payne <jim.payne@ametek.com>
2022-05-24 21:28:58 -0500

    Update menu building
    Prefer classes over interfaces. Simplify naming for clarity.

--- Cameca.CustomAnalysis.Interface/Menu/IMenuItemCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Cameca.CustomAnalysis.Interface;
-
-public interface IMenuItemCollection : IMenuItemBase
-{
-    string Header { get; }
-    IReadOnlyCollection<IMenuItemBase> ItemCollection { get; }
-}
--- Cameca.CustomAnalysis.Interface/Menu/IMenuSeparator.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Cameca.CustomAnalysis.Interface;
-
-public interface IMenuSeparator : IMenuItemBase { }
