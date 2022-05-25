d9edc033e1284f579d77c10ad2871503a1d34db1
Jim Payne <jim.payne@ametek.com>
2022-05-24 21:45:45 -0500

    Add node visibility control resource
    Implementations control a checkbox in the APS analysis tree.
    Checkbox is intended to be used to control associated visibility
    in the main 3D chart if applicable.

---
+++ Cameca.CustomAnalysis.Interface/Resources/INodeVisibilityControl.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface INodeVisibilityControl : INotifyPropertyChanged
+{
+    bool IsEnabled { get; set; }
+    bool IsVisible { get; set; }
+}
