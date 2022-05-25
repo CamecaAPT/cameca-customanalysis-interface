2cd06b3e9b23bcab7d19ddfa750ecf73aafbed5e
Jim Payne <jim.payne@ametek.com>
2022-05-25 11:34:16 -0500

    Add known IonData object section names
    More may be added, but these initial values should be considered
    to be currently be guarenteed to be present and populated.

---
+++ Cameca.CustomAnalysis.Interface/Resources/IonData/IonDataSectionName.cs
@@ -0,0 +1,8 @@
+﻿namespace Cameca.CustomAnalysis.Interface.Resources.IonData;
+
+public static class IonDataSectionName
+{
+	public const string Position = "Position";
+	public const string Mass = "Mass";
+	public const string IonType = "Ion Type";
+}
