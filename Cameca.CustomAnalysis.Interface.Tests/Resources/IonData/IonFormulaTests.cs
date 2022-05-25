2546d1717a0d838895087dc9812503a5aa96f155
Jim Payne <jim.payne@ametek.com>
2022-05-25 00:23:21 -0500

    Fix IonFormula equality bug
    Add test project and IonFormula equality tests

---
+++ Cameca.CustomAnalysis.Interface.Tests/Resources/IonData/IonFormulaTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace Cameca.CustomAnalysis.Interface.Tests;
+
+public class IonFormulaTests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void IonFormulaEqualsTest()
+	{
+		var x = new IonFormula(new[] { new IonFormula.Component("Cu", 1) });
+		var y = new IonFormula(new[] { new IonFormula.Component("Cu", 1) });
+		Assert.AreEqual(x, y);
+	}
+
+	[Test]
+	public void IonFormulaEqualsOperatorTest()
+	{
+		var x = new IonFormula(new[] { new IonFormula.Component("Cu", 1) });
+		var y = new IonFormula(new[] { new IonFormula.Component("Cu", 1) });
+		Assert.IsTrue(x == y);
+	}
+
+	[Test]
+	public void IonFormulaNotEqualsNameTest()
+	{
+		var x = new IonFormula(new[] { new IonFormula.Component("Cu", 1) });
+		var y = new IonFormula(new[] { new IonFormula.Component("O", 1) });
+		Assert.AreNotEqual(x, y);
+	}
+
+	[Test]
+	public void IonFormulaNotEqualsOperatorNameTest()
+	{
+		var x = new IonFormula(new[] { new IonFormula.Component("Cu", 1) });
+		var y = new IonFormula(new[] { new IonFormula.Component("O", 1) });
+		Assert.IsTrue(x != y);
+	}
+
+	[Test]
+	public void IonFormulaNotEqualsCountTest()
+	{
+		var x = new IonFormula(new[] { new IonFormula.Component("O", 1) });
+		var y = new IonFormula(new[] { new IonFormula.Component("O", 2) });
+		Assert.AreNotEqual(x, y);
+	}
+
+	[Test]
+	public void IonFormulaNotEqualsOperatorCountTest()
+	{
+		var x = new IonFormula(new[] { new IonFormula.Component("O", 1) });
+		var y = new IonFormula(new[] { new IonFormula.Component("O", 2) });
+		Assert.IsTrue(x != y);
+	}
+}
