b329ddd384c96c37a5f4fc76b04393b3a261aea7
Jim Payne <jim.payne@ametek.com>
2022-05-27 01:16:22 -0500

    Add progress dialog resource
    AP Suite has convenience tools for displaying a modal progress
    dialog to wrap asyncronous or long-running actions.
    
    While this functionality could be replicated using native WPF controls
    and the Prisim provided dialog service, it can be difficult to make robust.
    
    Providing the existing functionality as a resource allows for easier
    development of a custom analysis that includes long-running work,
    and additionally ensures the displayed dialog will appear with
    native theme and style for integration with the main application.

---
+++ Cameca.CustomAnalysis.Interface/Resources/IProgressDialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface IProgressDialog
+{
+	Task ShowDialog(string? message, Func<Task> action);
+	Task ShowDialog(string? message, Func<IProgress<double>, Task> action);
+	Task ShowDialog(string? message, Func<CancellationToken, Task> action);
+	Task ShowDialog(string? message, Func<IProgress<double>, CancellationToken, Task> action);
+
+	Task<T> ShowDialog<T>(string? message, Func<Task<T>> action);
+	Task<T> ShowDialog<T>(string? message, Func<IProgress<double>, Task<T>> action);
+	Task<T> ShowDialog<T>(string? message, Func<CancellationToken, Task<T>> action);
+	Task<T> ShowDialog<T>(string? message, Func<IProgress<double>, CancellationToken, Task<T>> action);
+}
