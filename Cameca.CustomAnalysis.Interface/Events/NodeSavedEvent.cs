b3636c7090bb20a58b6f1faac7c9dbe5b4535c3a
Jim Payne <jim.payne@ametek.com>
2022-05-30 20:45:40 -0500

    Add on saved event

---
+++ Cameca.CustomAnalysis.Interface/Events/NodeSavedEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Prism.Events;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public class NodeSavedEvent : PubSubEvent<NodeSavedEventArgs> { }
+
+public class NodeSavedEventArgs : INodeTargetEvent, INodeInstantiatedEventArgs
+{
+	public Guid NodeId { get; }
+
+	public NodeSavedEventArgs(Guid analysisNodeId)
+	{
+		NodeId = analysisNodeId;
+	}
+}
+
+public static class NodeSavedEventExtensions
+{
+	public static void PublishNodeSaved(
+		this IEventAggregator eventAggregator,
+		Guid analysisNodeId)
+	{
+		eventAggregator
+			.GetEvent<NodeSavedEvent>()
+			.Publish(new NodeSavedEventArgs(analysisNodeId));
+	}
+
+	public static SubscriptionToken SubscribeNodeSaved(
+		this IEventAggregator eventAggregator,
+		Action<NodeSavedEventArgs> action,
+		Predicate<NodeSavedEventArgs>? filter = null)
+	{
+		return eventAggregator
+			.GetEvent<NodeSavedEvent>()
+			.Subscribe(action, filter: filter);
+	}
+}
+
