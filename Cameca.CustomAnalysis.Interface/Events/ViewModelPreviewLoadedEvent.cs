1c8b20434da81b7ad94c24594029c1df737c1934
Jim Payne <jim.payne@ametek.com>
2022-06-07 12:44:40 -0500

    Add view model preview loaded event

---
+++ Cameca.CustomAnalysis.Interface/Events/ViewModelPreviewLoadedEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Prism.Events;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public class ViewModelPreviewLoadedEvent : PubSubEvent<ViewModelPreviewLoadedEventArgs> { }
+
+public class ViewModelPreviewLoadedEventArgs : IViewModelTargetEvent
+{
+	public string ViewModelName { get; }
+
+	public Guid ViewModelId { get; }
+
+	public byte[]? Data { get; }
+
+	public ViewModelPreviewLoadedEventArgs(string viewModelName, Guid analysisViewModelId, byte[]? data = null)
+	{
+		ViewModelName = viewModelName;
+		ViewModelId = analysisViewModelId;
+		Data = data;
+	}
+}
+
+public static class ViewModelPreviewLoadedEventExtensions
+{
+	public static void PublishViewModelPreviewLoaded(this IEventAggregator eventAggregator,
+		string viewModelName,
+		Guid viewModelId,
+		byte[]? data = null)
+	{
+		eventAggregator
+			.GetEvent<ViewModelPreviewLoadedEvent>()
+			.Publish(new ViewModelPreviewLoadedEventArgs(viewModelName, viewModelId, data));
+	}
+
+	public static SubscriptionToken SubscribeViewModelPreviewLoaded(
+		this IEventAggregator eventAggregator,
+		Action<ViewModelPreviewLoadedEventArgs> action,
+		Predicate<ViewModelPreviewLoadedEventArgs>? filter = null)
+	{
+		return eventAggregator
+			.GetEvent<ViewModelPreviewLoadedEvent>()
+			.Subscribe(action, filter: filter);
+	}
+}
