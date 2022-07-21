dd1d9fed61e716fcd7ade6bf768ee172a8a5d920
Jim Payne <jim.payne@ametek.com>
2022-07-21 13:48:19 -0500

    Add reconstruction sections resource
    Allow adding msising reconstruciton section data if source information is availalbe

---
+++ Cameca.CustomAnalysis.Interface/Resources/IReconstructionSections.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface IReconstructionSections
+{
+	bool IsAddSectionAvailable { get; }
+	Task<IEnumerable<ReconstructionSectionInfo>> GetAvailableSections(IProgress<double>? progress = null, CancellationToken cancellationToken = default);
+	Task<bool> AddVirtualSections(IEnumerable<string> sectionNames, IProgress<double>? progress = null, CancellationToken cancellationToken = default);
+}
+
+public record ReconstructionSectionInfo(string Name, string Description);
---
+++ Cameca.CustomAnalysis.Interface/Resources/Providers/IReconstructionSectionsProvider.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface IReconstructionSectionsProvider
+{
+	IReconstructionSections? Resolve(Guid nodeId);
+}
