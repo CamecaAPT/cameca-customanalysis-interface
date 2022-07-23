43172d385ca39b5cdfaaaa91033ffa53518c0405
Jim Payne <jim.payne@ametek.com>
2022-07-23 00:24:24 -0500

    Menu support updates
    * Add Action location for non-persistent extensions
    * Correct file name to match contents
    * Add enabled flag and tooltip support to menu items

--- Cameca.CustomAnalysis.Interface/Menu/MenuAction.cs
+++ Cameca.CustomAnalysis.Interface/Menu/MenuAction.cs
@@ -6,14 +6,22 @@ namespace Cameca.CustomAnalysis.Interface;
 
 public class MenuAction : IMenuItem
 {
-    public string Caption { get; }
-    public ImageSource? Icon { get; }
-    public ICommand Command { get; }
+	public string Caption { get; }
 
-    public MenuAction(string caption, ICommand command, ImageSource? icon = null)
+	public ICommand Command { get; }
+
+	public ImageSource? Icon { get; }
+
+    public bool IsEnabled { get; }
+
+    public string? ToolTip { get; }
+
+    public MenuAction(string caption, ICommand command, ImageSource? icon = null, bool isEnabled = true, string? toolTip = null)
     {
 	    Caption = caption;
 	    Icon = icon;
 	    Command = command;
+	    IsEnabled = isEnabled;
+	    ToolTip = toolTip;
     }
 }
--- Cameca.CustomAnalysis.Interface/Menu/SubMenu.cs
+++ Cameca.CustomAnalysis.Interface/Menu/SubMenu.cs
@@ -5,11 +5,18 @@ namespace Cameca.CustomAnalysis.Interface;
 
 public class SubMenu : IMenuItem
 {
-    public string Caption { get; }
-    public IReadOnlyCollection<IMenuItem> MenuItems { get; init; } = Array.Empty<IMenuItem>();
+	public string Caption { get; }
 
-    public SubMenu(string caption)
+	public bool IsEnabled { get; }
+
+	public IReadOnlyCollection<IMenuItem> MenuItems { get; init; } = Array.Empty<IMenuItem>();
+	
+    public string? ToolTip { get; }
+
+	public SubMenu(string caption, bool isEnabled = true, string? toolTip = null)
     {
 	    Caption = caption;
+	    IsEnabled = isEnabled;
+	    ToolTip = toolTip;
     }
 }
