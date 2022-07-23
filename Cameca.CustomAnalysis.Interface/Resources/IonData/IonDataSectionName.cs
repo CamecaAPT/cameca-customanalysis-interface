545229f341589b36e0ea994b311f97b8d5bda8e0
Jim Payne <jim.payne@ametek.com>
2022-07-23 02:35:09 -0500

    Add additional known sections

--- Cameca.CustomAnalysis.Interface/Resources/IonData/IonDataSectionName.cs
+++ Cameca.CustomAnalysis.Interface/Resources/IonData/IonDataSectionName.cs
@@ -5,4 +5,11 @@ public static class IonDataSectionName
 	public const string Position = "Position";
 	public const string Mass = "Mass";
 	public const string IonType = "Ion Type";
+	public const string TimeOfFlight = "Time of Flight";
+	public const string Voltage = "Voltage";
+	public const string PulseVoltage = "Pulse Voltage";
+	public const string DetectorCoordinates = "Detector Coordinates";
+	public const string PulseDelta = "Pulse Delta";
+	public const string HitType = "Hit Type";
+	public const string Phase = "Phase";
 }
