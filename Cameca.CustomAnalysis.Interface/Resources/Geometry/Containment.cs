ad028969d0b23695fb24e7ff180075830a87d9a2
Jim Payne <jim.payne@ametek.com>
2023-06-19 10:18:26 -0500

    Add geometric ROI information support
    Geometric ROI nodes should return a more specific
    IGeometricRoiNode extension of INodeInfo. This will
    expose geometric region parameter including shape
    and containment information.

---
+++ Cameca.CustomAnalysis.Interface/Resources/Geometry/Containment.cs
@@ -0,0 +1,8 @@
+namespace Cameca.CustomAnalysis.Interface;
+
+public enum Containment
+{
+    Unknown = 0,
+    Interior = 1,
+    Exterior = 2,
+}
---
+++ Cameca.CustomAnalysis.Interface/Resources/Geometry/Shape.cs
@@ -0,0 +1,14 @@
+﻿namespace Cameca.CustomAnalysis.Interface;
+
+public enum Shape
+{
+	Unknown = 0,
+	Cube = 1,
+	Cylinder = 2,
+	Sphere = 3,
+	Torus = 4,
+	Shell = 5,
+	Trapezoid = 6,
+	Cap = 7,
+	Custom = 9,
+}
