dee2c90f25438928e8e6fb83397378b68e37f9a3
Jim Payne <jim.payne@ametek.com>
2023-07-25 12:32:43 -0500

    Add ion formula isotope calculator

---
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/FormulaIsotopeCalculator/IIonFormulaIsotopeCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface IIonFormulaIsotopeCalculator
+{
+	/// <summary>
+	/// Returns list of complex isotopes for a formula.
+	/// </summary>
+	/// <remarks>
+	///	Compute all mass and abundances for A_nB_m... where A,B,... are elements; n,m... are
+	///	their respective numbers in the complex. Furthermore, Each element has A_i, B_i... isotopes.
+	///
+	///  The calculation takes two steps:  First compute the the isotope abundances and masses for the
+	///  sub-complexes: A_n, B_m, etc.  Once the sub-complex isotope abundances and masses are computed,
+	///  these get combined to form the actual complex isotope masses and abundances.
+	///
+	///  After each step, the degenerate isotopes are combined.  Two isotopes (complex or subcomplex)
+	///  are degenerate if they have the same Atomic Mass number (A) - in principle they will have different
+	///  masses, but this is well beyond the resolving power of an atom probe.
+	/// </remarks>
+	/// <param name="ionFormula"></param>
+	/// <param name="options"></param>
+	/// <returns></returns>
+	IEnumerable<IIsotope> GetIsotopes(IonFormula ionFormula, IonFormulaIsotopeOptions? options = null);
+}
---
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/FormulaIsotopeCalculator/IonFormulaIsotopeOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+/// <summary>
+/// Filtering options for calculating complex formula isotopes.
+/// </summary>
+/// <remarks>
+/// First removes isotopes with abundance less than <see cref="MinimumIsotopeAbundance"/>,
+/// the filters to a maximum of <see cref="MaxIsotopeCount"/> isotopes if specified.
+/// </remarks>
+public class IonFormulaIsotopeOptions
+{
+	/// <summary>
+	/// Limit to a maximum number of this many isotopes.
+	/// </summary>
+	public int? MaxIsotopeCount { get; init; } = null;
+
+	/// <summary>
+	/// Remove itotopes with minimum abundance less than this value.
+	/// </summary>
+	public double MinimumIsotopeAbundance { get; init; } = 0d;
+
+	/// <summary>
+	/// Id of the element data set to use for isotope information for the calculation.
+	/// If null, the application default element data set will be used.
+	/// </summary>
+	/// <remarks>
+	/// See <see cref="INodeElementDataSet.ElementDataSetId"/> for retrieving the current element data set,
+	/// or <see cref="IElementDataSetService"/> for retrieving specific element data sets.
+	/// </remarks>
+	public int? ElementDataSet { get; init; } = null;
+
+	/// <summary>
+	/// Normaize remaining isotope abundances after any filtering to sum to 1.
+	/// </summary>
+	public bool NormalizeAbundances { get; set; } = true;
+}
