4d4da486ed9c0e6f1d7c53056a0e15bde70dfec1
Jim Payne <jim.payne@ametek.com>
2023-09-02 19:27:37 -0500

    Add grid 3d parameters
    Allows retrieval of relevant information regarding isosurfaces

---
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/Grid3D/IGrid3DParameters.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface IGrid3DParameters
+{
+	Vector3 VoxelSize { get; }
+	Vector3 BuildGrid { get; }
+	bool ErodeByDelocalization { get; }
+	bool DoFirstPassDeloc { get; }
+}
