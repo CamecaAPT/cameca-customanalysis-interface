ba9c6eedd6f319ad19c3339c333b70a8c61073eb
Jim Payne <jim.payne@ametek.com>
2023-09-02 19:25:13 -0500

    Add mass spectrum analysis interfaces

---
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/MassSpectrum/IHistogramData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface IHistogramData
+{
+	int BinCount { get; }
+	double BinWidth { get; }
+	double BinCenter { get; }
+	long TotalCount { get; }
+	ReadOnlyMemory<float> Values { get; set; }
+}
---
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/MassSpectrum/IMassSpectrumData.cs
@@ -0,0 +1,7 @@
+﻿namespace Cameca.CustomAnalysis.Interface;
+
+public interface IMassSpectrumData
+{
+	IHistogramData MassHistogram { get; set; }
+	IHistogramData BackgroundModel { get; set; }
+}
