da722494a74abf736c1f9ca8da1f5fbe245a328a
Jim Payne <jim.payne@ametek.com>
2023-09-03 00:44:31 -0500

    Peak decomp data needs to be a collection of records

--- Cameca.CustomAnalysis.Interface/Resources/Analyses/PeakDecomposition/IPeakDecompositionData.cs
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/PeakDecomposition/IPeakDecompositionData.cs
@@ -2,16 +2,5 @@
 
 public interface IPeakDecompositionData
 {
-	IonFormula Formula { get; }
-	int Charge { get; }
-	double Mass { get; }
-	double Low { get; }
-	double Upper { get; }
-	int UncorrectedCounts { get; }
-	int BackgroundCorrectedCounts { get; }
-	int DecomposedCounts { get; }
-	double DecompositionAbundance { get; }
-	double ExpectedAbundance { get; }
-	double EstimatedError { get; }
-	double StandardDeviation { get; }
+	IPeakDecompositionRecord[] Records { get; }
 }
