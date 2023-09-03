2c83910e8225e4fc09bc696d31c10d71b156606f
Jim Payne <jim.payne@ametek.com>
2023-09-02 21:48:43 -0500

    Fix histogram data interface
    BinCenter only makes sense as a method accepting a bin index.
    However it is trivially computed from a start value and the bin
    width as the histogram is fixed width.
    
    TotalCount is a contextual optimization in AP Suite and should not
    be exposed via extensions.
    
    BinCount is trivially give from Values.Length, and better to remove
    unnecessary duplication

--- Cameca.CustomAnalysis.Interface/Resources/Analyses/MassSpectrum/IHistogramData.cs
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/MassSpectrum/IHistogramData.cs
@@ -2,11 +2,15 @@
 
 namespace Cameca.CustomAnalysis.Interface;
 
+/// <summary>
+/// A fixed width histogram of floating point values.
+/// Histogram is defined by a starting value and a fixed bin step.
+/// Values are contiguous.
+/// Total histogram range is [Start, Start + BinWidth * Values.Length].
+/// </summary>
 public interface IHistogramData
 {
-	int BinCount { get; }
+	double Start { get; }
 	double BinWidth { get; }
-	double BinCenter { get; }
-	long TotalCount { get; }
 	ReadOnlyMemory<float> Values { get; }
 }
