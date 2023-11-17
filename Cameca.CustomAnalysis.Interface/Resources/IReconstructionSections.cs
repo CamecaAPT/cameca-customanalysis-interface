7b68283fc361313876a52f1f5aa2e1731d032eba
Jim Payne <jim.payne@ametek.com>
2023-11-17 17:01:17 -0600

    Inlude add reconstruction section persistance support

--- Cameca.CustomAnalysis.Interface/Resources/IReconstructionSections.cs
+++ Cameca.CustomAnalysis.Interface/Resources/IReconstructionSections.cs
@@ -9,6 +9,14 @@ public interface IReconstructionSections
 {
 	bool IsAddSectionAvailable { get; }
 	Task<IEnumerable<ReconstructionSectionInfo>> GetAvailableSections(IProgress<double>? progress = null, CancellationToken cancellationToken = default);
+	Task<bool> AddSections(IEnumerable<string> sectionNames, bool isVirtual = false, IProgress<double>? progress = null, CancellationToken cancellationToken = default)
+	{
+#pragma warning disable CS0618 // Type or member is obsolete
+		if (isVirtual) return AddVirtualSections(sectionNames, progress, cancellationToken);
+#pragma warning restore CS0618 // Type or member is obsolete
+		return Task.FromResult(false);
+	}
+	[Obsolete("Use AddSections with isVirtual: true")]
 	Task<bool> AddVirtualSections(IEnumerable<string> sectionNames, IProgress<double>? progress = null, CancellationToken cancellationToken = default);
 }
 
