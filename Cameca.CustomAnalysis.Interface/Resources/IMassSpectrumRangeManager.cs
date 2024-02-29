4e85ef031973ef19f9e87241fc4835d706fe4bdb
Jim Payne <jim.payne@ametek.com>
2024-02-29 11:20:38 -0600

    Remove unimplemented docstring

--- Cameca.CustomAnalysis.Interface/Resources/IMassSpectrumRangeManager.cs
+++ Cameca.CustomAnalysis.Interface/Resources/IMassSpectrumRangeManager.cs
@@ -8,11 +8,6 @@ public interface IMassSpectrumRangeManager
 	bool IsEditable { get; }
 	IDictionary<IonFormula, IonRangeDefinition> GetRanges();
 	Task<bool> SetRanges(IDictionary<IonFormula, IonRangeDefinition> ranges);
-	/// <summary>
-	/// Sets 
-	/// </summary>
-	/// <param name="ranges"></param>
-	/// <returns></returns>
 	bool SetRangesSync(IDictionary<IonFormula, IonRangeDefinition> ranges)
 		=> throw DefaultImplementationHelper.RequiresAPSuiteUpdate();
 }
