a6bf85afd4dbd864cd69ab5ae85b092012da0807
Jim Payne <jim.payne@ametek.com>
2025-01-22 14:13:03 -0600

    Add extension options feature
    Extensions can register an options model to integration with AP Suite
    options infrastructure, allowing editing and persisting global options
    for an extension in a standard and expected place in the application.
    
    Options are hosted under Configuration > Options in AP Suite like
    other native AP Suite options.
    
    Options can be registered using only a model, in which case a default
    editor is generated, and public properties of the options class can be
    customized with attributes to control the editor used for the basic data
    type. This currently support file and folder paths for string
    properties, both single or multiple selections supported. If multiple
    selections, the resulting string is delimited with Path.PathSeparator,
    by default a semicolon.
    
    Options can be accessed from the hosted analysis in the extension by
    injecting and using the IOptionsAccessor interface and requesting
    the registered options model.
    the IOptionsAccessor ser

--- Cameca.CustomAnalysis.Interface/IExtensionRegistry.cs
+++ Cameca.CustomAnalysis.Interface/IExtensionRegistry.cs
@@ -1,7 +1,19 @@
-﻿namespace Cameca.CustomAnalysis.Interface;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace Cameca.CustomAnalysis.Interface;
 
 public interface IExtensionRegistry
 {
 	void RegisterAnalysisView<TView, TViewModel>(AnalysisViewLocation location = AnalysisViewLocation.Default);
 	void RegisterPreview(string nodeUniqueId, string viewModelUniqueId);
+	void RegisterOptions<TOptionsModel>(string title, Version? version = null)
+		where TOptionsModel : class, new()
+		=> throw DefaultImplementationHelper.RequiresAPSuiteUpdate();
+	void RegisterOptions<TOptionsModel, TView, TViewModel>(string title, Version? version = null)
+		where TOptionsModel : class, IEquatable<TOptionsModel>, ICloneable, INotifyPropertyChanged, new()
+		where TView : Control, new()
+		where TViewModel : class, IOptionsViewModel<TOptionsModel>
+		=> throw DefaultImplementationHelper.RequiresAPSuiteUpdate();
 }
