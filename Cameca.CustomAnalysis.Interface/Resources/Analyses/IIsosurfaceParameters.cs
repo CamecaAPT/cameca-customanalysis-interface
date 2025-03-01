126f27f8b9427c26277591c7acaf006237e793ab
Jim Payne <jim.payne@ametek.com>
2025-02-28 22:46:05 -0600

    Add isosurface and interface parameters

---
+++ Cameca.CustomAnalysis.Interface/Resources/Analyses/IIsosurfaceParameters.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+
+namespace Cameca.CustomAnalysis.Interface;
+
+public interface IIsosurfaceParameters
+{
+	IonRatio IonRatio { get; }
+	ProfileType ProfileType { get; }
+	bool AtomicDecomposed { get; }
+	Color Color { get; }
+	float Isovalue { get; }
+	float IsovalueMin { get; }
+	float IsovalueMax { get; }
+	double SampleCountThreshold { get; }
+	double ConfidenceSigma { get; }
+	FillMissingType FillMissingType { get; }
+}
